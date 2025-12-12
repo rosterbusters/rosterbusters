420689dee272530f0e530ec6d2a8a022d8b57d38
taniakoh <taniakohtzeern@gmail.com>
2025-12-12 15:41:23 +0800

    added packages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import path from "node:path"
 import { tanstackRouter } from "@tanstack/router-plugin/vite"
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +16,7 @@ export default defineConfig({
       target: "react",
       autoCodeSplitting: true,
     }),
+    tailwindcss(),
     react(),
   ],
 })
