ad121862010770591dbd9d77d4ddc1b74e792fad
taniakoh <taniakohtzeern@gmail.com>
2026-01-24 03:22:16 +0800

    small updates, need to put placeholder for calend ar for backend

--- frontend/src/components/WardStaff/HomePage/StatusBanner.tsx
+++ frontend/src/components/WardStaff/HomePage/StatusBanner.tsx
@@ -1,29 +1,53 @@
-import { Heading, Stack} from "@chakra-ui/react";
+import { Heading, Stack, Highlight } from "@chakra-ui/react";
 
-const Name="John Doe";
+const Name = "John Doe";
+interface upcomingShiftProps {
+  shiftName: string;
+  shiftDate: string;
+  startTime: string;
+  endTime: string;
+}
+//get name
+const userName = "John Doe";
+//get upcoming shift details
+const upcomingShift: upcomingShiftProps = {
+  shiftName: "Day",
+  shiftDate: "Tuesday, 13/11/2001",
+  startTime: "9:00AM",
+  endTime: "5:00PM",
+};
 
 export default function StatusBanner() {
   return (
     <Stack
-                bgColor={"white"}
-                p={12}
-                width="100%"
-                rounded={"lg"}
-                height={"100%"}
-                alignItems={"start"}
-                justifyContent={"center"}
-              >
+      bgColor={"white"}
+      p={12}
+      width="100%"
+      rounded={"lg"}
+      height={"100%"}
+      alignItems={"start"}
+      justifyContent={"center"}
+    >
       <Heading size="2xl" color={"foreground"} fontWeight={"semibold"}>
         Hi
         <Heading size="2xl" as="span" fontWeight={"semibold"} color={"primary"}>
           {" "}
-          {Name},
+          {userName},
         </Heading>
       </Heading>
       <Heading size="2xl" color={"foreground"} fontWeight={"semibold"}>
-        {" "}
-        You have an upcoming 
-        Day Shift at 9:00AM, on Tuesday, 13/11/2001.
+        You have an upcoming{" "}
+        <Heading as="span" size="2xl" fontWeight={"semibold"} color={"primary"}>
+          {upcomingShift?.shiftName} Shift {" "}
+        </Heading>
+        at
+        <Heading as="span" size="2xl" fontWeight={"semibold"} color={"primary"}>
+          {" "}{upcomingShift?.startTime}
+        </Heading>
+        , on {" "}
+        <Heading as="span" size="2xl" fontWeight={"semibold"} color={"primary"}>
+          {upcomingShift?.shiftDate}.
+        </Heading>
       </Heading>
     </Stack>
   );
