feddb0e9eb33f1ba0e592fec8552ab525b171cca
taniakoh <taniakohtzeern@gmail.com>
2026-01-31 01:00:11 +0800

    done with the creating dialog: need to implement editing dialog, handle on creation

--- frontend/src/routes/_wardStaff/request-application.tsx
+++ frontend/src/routes/_wardStaff/request-application.tsx
@@ -13,9 +13,12 @@ import {
   Badge,
   Button,
 } from "@chakra-ui/react";
+import {Plus} from "lucide-react"
 import { useState } from "react";
+
 import { AssignableStatus } from "@/components/WardStaff/Requests/AssignableStatus";
 import RequestCalendar from "@/components/WardStaff/Requests/RequestCalendar";
+import { NewShiftRequest } from "@/components/WardStaff/Requests/NewShiftRequest";
 
 export const Route = createFileRoute("/_wardStaff/request-application")({
   component: RouteComponent,
@@ -29,8 +32,8 @@ function handleLeaveClicked() {
 }
 
 function RouteComponent() {
-  const [shiftReqApplicationOpen,setshiftReqApplicationOpen]=useState(false);
-  
+  const [isShiftRequestOpen, setIsShiftRequestOpen] = useState(false);
+
   return (
     <Flex
       minH="100vh"
@@ -51,14 +54,18 @@ function RouteComponent() {
         textAlign={"center"}
       >
         <Text color="primary" fontWeight="semibold" fontSize={"lg"}>Leave and Shift Request Application</Text>
-        <HStack gap={0}>
-          <Button disabled variant={"outline"} onClick={handleShiftClicked} roundedTopLeft="full" roundedBottomLeft="full">
-            Shift Requests
-          </Button>
-          <Button variant={"outline"} onClick={handleLeaveClicked} roundedBottomRight="full" roundedTopRight="full">
-            Leave Requests
-          </Button>
-        </HStack>
+        <Grid templateColumns="1fr auto 1fr" w="full" alignItems="center">
+          <GridItem />
+          <HStack gap={0} justifySelf="center">
+            <Button disabled variant={"outline"} onClick={handleShiftClicked} roundedTopLeft="full" roundedBottomLeft="full">
+              Shift Requests
+            </Button>
+            <Button variant={"outline"} onClick={handleLeaveClicked} roundedBottomRight="full" roundedTopRight="full">
+              Leave Requests
+            </Button>
+          </HStack>
+          <Button variant={"outline"} justifySelf="end" size="sm" onClick={() => setIsShiftRequestOpen(true)}><Plus/>Add Shift Request</Button>
+        </Grid>
         <Grid templateColumns={{base:'1fr', md:"1fr auto 1fr"}} w="full" gap={{base:2, md:0}}>
           <GridItem />
           <Text color="foreground" fontWeight="light" justifySelf="center">
@@ -72,6 +79,10 @@ function RouteComponent() {
         <RequestCalendar/>
         </Box>
       </VStack>
+      <NewShiftRequest
+        isOpen={isShiftRequestOpen}
+        onClose={() => setIsShiftRequestOpen(false)}
+      />
     </Flex>
   );
 }
