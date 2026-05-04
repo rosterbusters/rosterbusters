5aa1de808515e83d50803450b7d4e824b33819f9
Tania Koh Tze Ern <taniakohtzeern@gmail.com>
2026-05-05 05:42:16 +0800

    fix build issues

--- frontend/src/routes/ward-staff/home.tsx
+++ frontend/src/routes/ward-staff/home.tsx
@@ -43,7 +43,7 @@ function HomePage() {
           rounded="lg"
           width={{ base: "100%", md: "50%" }}
         >
-          <NotificationBannerContainer />
+          <NotificationBannerContainer role="nurse" />
         </Stack>
       </Stack>
 
